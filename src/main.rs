d5f65232f507062e1722832bcf9e4eac0f4857c0
Alvaro Muhlethaler <alvaro.muhlethaler@gmail.com>
2021-04-16 13:55:42 +0100

    Adds Subscribe endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+use std::net::TcpListener;
+use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8090")?
-    .run()
-    .await
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to start in port 80000");
+    run(listener)?.await
 }
