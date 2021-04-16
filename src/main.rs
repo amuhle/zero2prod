2ac8cf09d6a94a598bd064cde9d760f690472615
Alvaro Muhlethaler <alvaro.muhlethaler@gmail.com>
2021-04-16 16:28:56 +0100

    Separates logic in different files

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to start in port 80000");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address).expect("Failed to start in port 80000");
     run(listener)?.await
 }
