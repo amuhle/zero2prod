4b6c6713abe4e09190ee3a0454efc9d658c4cbb4
Alvaro Muhlethaler <alvaro.muhlethaler@gmail.com>
2021-05-16 11:44:24 +0100

    Uses tracing instead of log

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -14,13 +14,21 @@ pub async fn subscribe(
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, HttpResponse> {
     let request_id = Uuid::new_v4();
-    log::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
+
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %request_id,
+        email = %form.email,
+        name = %form.name,
     );
-    log::info!("request_id {} - Saving new subscriber details in the database", request_id);
+
+    let _request_span_guard = request_span.enter();
+
+    tracing::info_span!(
+        "Saving new subscriber details in the database", 
+        %request_id
+    );
+
     sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)"#,
@@ -32,9 +40,10 @@ pub async fn subscribe(
     .execute(pool.get_ref())
     .await
     .map_err(|e| {
-        log::error!("Failed to execute query: {:?}", e);
+        tracing::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
         HttpResponse::InternalServerError().finish()
     })?;
-    log::info!("request_id {} - New subscriber details have been saved", request_id);
+    tracing::info!("request_id {} - New subscriber details have been saved", request_id);
+
     Ok(HttpResponse::Ok().finish())
 }
