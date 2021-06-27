d77745036cdcaf8d258e5a1a9932453fa447c824
Alvaro Muhlethaler <alvaro.muhlethaler@gmail.com>
2021-06-28 00:16:53 +0100

    Refactors logging

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,7 +2,6 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
-use tracing_futures::Instrument;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -10,24 +9,32 @@ pub struct FormData {
     name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        email = %form.email,
+        name = %form.name
+    )
+)]
 pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, HttpResponse> {
-    let request_id = Uuid::new_v4();
 
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber",
-        %request_id,
-        email = %form.email,
-        name = %form.name,
-    );
-    let _request_span_guard = request_span.enter();
+    insert_subscriber(&pool, &form)
+    .await
+    .map_err(|_| HttpResponse::InternalServerError().finish())?;
 
-    let query_span = tracing::info_span!(
-        "Saving new subscriber details in the database", 
-        %request_id
-    );
+    Ok(HttpResponse::Ok().finish())
+}
+
+#[tracing::instrument(name="Saving new subscriber details in the DB", skip(form, pool))]
+pub async fn insert_subscriber(
+    pool:&PgPool,
+    form: &FormData,
+) -> Result<(), sqlx::Error> {
 
     sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -37,14 +44,12 @@ pub async fn subscribe(
         form.name,
         Utc::now(),
     )
-    .execute(pool.get_ref())
-    .instrument(query_span)
+    .execute(pool)
     .await
     .map_err(|e| {
-        tracing::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
-        HttpResponse::InternalServerError().finish()
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
     })?;
-    tracing::info!("request_id {} - New subscriber details have been saved", request_id);
 
-    Ok(HttpResponse::Ok().finish())
+    Ok(())
 }
