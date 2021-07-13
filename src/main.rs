eb5d71a060af1c5db284e27e983e9a367303831d
Alvaro Muhlethaler <alvaro.muhlethaler@gmail.com>
2021-07-13 22:39:33 +0100

    Adds docker config

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
-use sqlx::PgPool;
-use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use sqlx::PgPool;
+use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     // Use info-level or above if RUST_LOG env var has not been set
     let subscriber = get_subscriber("zero2prod".into(), "info".into());
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_string = configuration.database.connection_string();
-    let connection_pool = PgPool::connect(&connection_string)
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Db.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
-
     let listener = TcpListener::bind(address).expect("Failed to start in port 80000");
     run(listener, connection_pool)?.await
 }
